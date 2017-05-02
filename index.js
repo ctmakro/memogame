48f151f016c81603800ae061c3fed85827515825
ctmakro <ctmakro@gmail.com>
2017-05-03 04:08:20 +0800

    completely refactored using new class maker

--- index.js
+++ index.js
@@ -10,237 +10,239 @@ var range = function (i) {
     return arr;
 };
 var choice = function (i) { return Math.floor(Math.random() * i); };
+var make = (function () {
+    var make = function (populator) {
+        var c = function () { this.__init__(this, arguments); };
+        if (typeof populator === 'function')
+            populator(c.prototype);
+        else
+            c.prototype = populator;
+        return c;
+    };
+    var make_new = function (new_populator) {
+        var nc = make(new_populator);
+        nc.prototype.__proto__ = this.prototype;
+        nc.prototype.super = this.prototype;
+        return nc;
+    };
+    Function.prototype.make = make_new;
+    return make;
+})();
 var board = geid('board');
 var buttons = geid('buttons');
-var make_randomcolor = function () {
-    var self = {};
-    self.resample = function () {
+var RandomColorGenerator = make(function (p) {
+    var c2str = function (c) { return ("rgb(" + c[0] + "," + c[1] + "," + c[2] + ")"); };
+    p.resample = function () {
         while (1) {
             var c = [choice(100) + 60, choice(80) + 60, choice(120) + 60];
             var brightness = c[0] * 0.3 + c[1] * .58 + c[2] * 0.12;
             if (brightness > 100 && brightness < 150)
                 break; // reject colors that are too dark
         }
-        self.c = c;
+        this.c = c;
     };
-    var to_string = function (c) { return ("rgb(" + c[0] + "," + c[1] + "," + c[2] + ")"); };
-    self.light = function () { return to_string(self.c); };
-    self.dark = function () { return to_string(self.c.map(function (n) { return Math.floor(n * 0.89); })); };
-    self.resample();
-    return self;
-};
-var randomcolor = make_randomcolor();
-var make_button = function () {
-    var self = {};
-    self.elem = ce('div');
-    buttons.appendChild(self.elem);
-    self.text = '';
-    self.elem.className = 'button-base box-base shadow-base';
-    self.onclick = function (f) {
-        var g = function () { return f(self); };
-        self.elem.addEventListener('click', g);
+    p.__init__ = function () { this.resample(); };
+    p.light = function () { return c2str(this.c); };
+    p.dark = function () { return c2str(this.c.map(function (n) { return Math.floor(n * 0.89); })); };
+});
+var randomcolor = new RandomColorGenerator();
+var Box = make(function (p) {
+    p.__init__ = function (parent) {
+        this.elem = ce('div');
+        if (parent)
+            parent.appendChild(this.elem);
+        this.text = '';
+        this.classes = ['box-base'];
     };
-    self.render = function () {
-        self.elem.innerHTML = self.text;
+    p.render = function () {
+        this.elem.className = this.classes.join(' ');
+        this.elem.innerHTML = this.text;
     };
-    self.render();
-    return self;
-};
-var make_brick = function () {
-    var self = {};
-    // create the brick and add to board
-    self.elem = ce('div');
-    self.elem2 = ce('div');
-    self.elem3 = ce('div');
-    self.elem.className = 'brick-base-outer';
-    self.elem3.className = 'brick-base-inner';
-    self.elem2.appendChild(self.elem3);
-    self.elem.appendChild(self.elem2);
-    board.appendChild(self.elem);
-    self.set_mode = function (mode) {
-        // mode could be one of
-        // empty, displaying, covered, revealed
-        self.mode = mode;
+    p.onclick = function (f) {
+        var _this = this;
+        var g = function () { return f(_this); };
+        this.elem.addEventListener('click', g);
     };
-    self.set_number = function (number) {
-        self.number = number;
+});
+var Button = Box.make(function (p) {
+    p.__init__ = function () {
+        this.super.__init__(buttons);
+        this.classes = this.classes.concat(['button-base', 'shadow-base']);
     };
-    self.render = function () {
-        // use mode string as class string.
-        self.elem2.className = self.mode + ' brick-base box-base ' +
-            (self.mode == 'empty' || self.mode == 'displaying' ? '' : 'shadow-base');
-        self.elem2.style.color = (self.mode == 'revealed-wrong' ? randomcolor.light() : '');
-        if (self.number !== 0 && self.mode !== 'covered') {
-            self.elem3.innerHTML = self.number.toString();
+});
+var Brick = Box.make(function (p) {
+    p.__init__ = function () {
+        this.super.__init__(board); // this.elem created
+        this.elem2 = ce('div');
+        this.elem3 = ce('div');
+        this.elem.className = 'brick-base-outer';
+        this.elem3.className = 'brick-base-inner';
+        this.elem2.appendChild(this.elem3);
+        this.elem.appendChild(this.elem2);
+        this.mode = 'empty';
+        this.number = 0;
+    };
+    p.set_mode = function (mode) {
+        this.mode = mode;
+    };
+    p.set_number = function (number) {
+        this.number = number;
+    };
+    p.render = function () {
+        var classes = [this.mode, 'brick-base']
+            .concat(this.classes)
+            .concat([(this.mode == 'empty' || this.mode == 'displaying' ? '' : 'shadow-base')]);
+        this.elem2.className = classes.join(' ');
+        this.elem2.style.color = (this.mode == 'revealed-wrong' ?
+            randomcolor.light() : '');
+        if (this.number !== 0 && this.mode !== 'covered') {
+            this.elem3.innerHTML = this.number.toString();
         }
         else {
-            self.elem3.innerHTML = ' ';
+            this.elem3.innerHTML = '';
         }
     };
-    self.onclick = function (f) {
-        var g = function () { return f(self); };
-        self.elem.addEventListener('click', g);
+});
+var StateMachine = make(function (p) {
+    p.__init__ = function () {
+        var _this = this;
+        this.level = 2;
+        this.state = 'idle';
+        this.state_counter = 1;
+        var num_of_bricks = 36;
+        this.bricks =
+            range(num_of_bricks).map(function (i) {
+                var b = new Brick();
+                b.index = i;
+                b.onclick(function (b) { return _this.brick_click(b); });
+                return b;
+            });
+        this.btn_start = new Button();
+        this.btn_start.onclick(function (b) { return _this.btn_start_click(b); });
+        this.render();
     };
-    self.set_mode('empty');
-    self.set_number(0);
-    return self;
-};
-var make_state_machine = function (num_of_bricks) {
-    var self = {};
-    self.level = 2;
-    self.state = 'idle';
-    self.state_counter = 1;
-    self.bricks = [];
-    for (var _i = 0, _a = range(num_of_bricks); _i < _a.length; _i++) {
-        i = _a[_i];
-        var b = make_brick();
-        b.index = i;
-        b.onclick(function (brick) {
-            self.brick_click(brick);
-        });
-        self.bricks.push(b);
-    }
-    self.btn_start = make_button();
-    self.btn_start.onclick(function (btn) {
-        self.btn_start_click(btn);
-    });
-    ///////////////////////
-    self.clear_board = function () {
-        self.bricks.map(function (b) {
+    p.clear_board = function () {
+        this.bricks.map(function (b) {
             b.set_mode('empty');
             b.set_number(0);
         });
-        self.render();
     };
-    self.calc_observation_time = function () {
-        return (self.level + 1) * 0.7;
+    p.calc_observation_time = function () {
+        return (this.level - 1) * 0.7;
     };
-    self.init_board = function () {
-        self.clear_board();
-        for (var _i = 0, _a = range(self.level); _i < _a.length; _i++) {
-            i = _a[_i];
-            var k = 1 + i;
-            //loop k from 1 to level
+    p.init_board = function () {
+        var _this = this;
+        this.clear_board();
+        range(this.level).map(function (i) {
+            //loop i from 0 to level-1
             while (1) {
-                var index = choice(self.bricks.length);
-                var b = self.bricks[index];
+                var b = _this.bricks[choice(_this.bricks.length)];
                 if (b.number == 0) {
-                    // if block have not been assigned b4
+                    // if block have not been assigned anything
                     b.set_mode('displaying');
-                    b.set_number(k);
+                    b.set_number(i + 1);
                     break;
                 }
             }
-        }
-        self.state = 'displaying';
-        self.state_counter = 1;
-        self.render();
-        self.timeout_handle = setTimeout(self.cover_all, Math.floor(1000 * self.calc_observation_time()));
-    };
-    self.cover_all = function () {
-        if (self.state == 'displaying') {
-            clearTimeout(self.timeout_handle);
-            self.bricks.map(function (b) {
-                if (b.number !== 0 && b.mode !== 'revealed')
-                    b.set_mode('covered');
-            });
-            self.state = 'waiting';
-            self.render();
-        }
-    };
-    self.reveal_all = function () {
-        self.bricks.map(function (b) {
-            if (b.mode == 'covered') {
-                b.set_mode('revealed-wrong');
-            }
         });
+        this.state = 'displaying';
+        this.state_counter = 1;
+        this.timeout_handle = setTimeout(function () { return _this.cover_all(); }, Math.floor(1000 * this.calc_observation_time()));
     };
-    self.brick_click = function (brick) {
-        switch (self.state) {
+    p.cover_all = function () {
+        if (this.state == 'displaying') {
+            clearTimeout(this.timeout_handle);
+            this.bricks
+                .filter(function (b) { return b.number !== 0 && b.mode !== 'revealed'; })
+                .map(function (b) { b.set_mode('covered'); });
+            this.state = 'waiting';
+            this.render();
+        }
+    };
+    p.reveal_all = function () {
+        this.bricks
+            .filter(function (b) { return b.mode == 'covered'; })
+            .map(function (b) { b.set_mode('revealed-wrong'); });
+    };
+    p.brick_click = function (brick) {
+        switch (this.state) {
             case 'displaying':
-                if (brick.number == self.state_counter) {
-                    self.state_counter++;
-                    brick.set_mode('revealed');
-                    self.cover_all();
+                {
+                    if (brick.number == this.state_counter) {
+                        this.state_counter++;
+                        brick.set_mode('revealed');
+                        this.cover_all();
+                        this.render();
+                    }
                 }
                 break;
             case 'waiting':
-                if (brick.number !== 0 && brick.mode == 'covered') {
-                    //brick is valid with number assigned && covered
-                    brick.set_mode('revealed');
-                    if (brick.number === self.state_counter) {
-                        // pressing the right brick
-                        if (self.level === self.state_counter) {
-                            //winning
-                            self.state = 'won';
-                            if (self.level >= 35) {
-                                self.state = 'won_fin';
+                {
+                    if (brick.number !== 0 && brick.mode == 'covered') {
+                        //brick is valid with number assigned && covered
+                        brick.set_mode('revealed');
+                        if (brick.number === this.state_counter) {
+                            // pressing the right brick
+                            if (this.level === this.state_counter) {
+                                //winning
+                                this.state = 'won';
+                                if (this.level >= 35) {
+                                    this.state = 'won_fin';
+                                }
+                            }
+                            else {
+                                this.state_counter++;
                             }
                         }
                         else {
-                            self.state_counter++;
+                            this.state = 'fail';
+                            brick.set_mode('revealed-wrong');
+                            this.reveal_all();
                         }
+                        this.render();
                     }
                     else {
-                        self.state = 'fail';
-                        brick.set_mode('revealed-wrong');
-                        self.reveal_all();
                     }
-                    self.render();
-                }
-                else {
                 }
                 break;
             default: break;
         }
     };
-    self.btn_start_click = function (btn) {
-        switch (self.state) {
+    p.btn_start_click = function (btn) {
+        switch (this.state) {
             case 'won':
-                self.level++;
+                this.level++;
             case 'fail':
             case 'idle':
-                self.init_board();
+                this.init_board();
                 break;
             case 'won_fin':
-                self.state = 'idle';
-                randomcolor.resample();
-                self.level = 2;
-                self.clear_board();
+                {
+                    this.state = 'idle';
+                    randomcolor.resample();
+                    this.level = 2;
+                    this.clear_board();
+                }
                 break;
-            case 'displaying':
-                break;
+            case 'displaying': break;
             default:
-                self.init_board();
+                this.init_board();
                 break;
         }
+        this.render();
     };
-    self.render = function () {
+    p.render = function (btn) {
         // state application
-        self.btn_start.text = {
+        this.btn_start.text = {
             idle: 'Start',
             won: 'Next Level',
             won_fin: 'Congratulations! Click to Restart',
             fail: 'Try Again'
-        }[self.state] || 'Restart';
+        }[this.state] || 'Restart';
+        this.btn_start.render();
         document.body.style.backgroundColor = randomcolor.light();
         geid('author').style.color = randomcolor.dark();
-        self.bricks.map(function (b) {
-            b.render();
-        });
-        self.btn_start.render();
+        this.bricks.map(function (b) { b.render(); });
     };
-    self.render();
-    return self;
-};
-var make_app = function () {
-    var self = {};
-    var num_of_bricks = 36;
-    self.init = function () {
-        // create the state machine
-        self.state_machine = make_state_machine(num_of_bricks);
-    };
-    self.init();
-    return self;
-};
-var app = make_app();
+});
+var sm = new StateMachine();
